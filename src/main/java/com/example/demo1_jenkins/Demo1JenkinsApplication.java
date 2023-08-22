0bf3579b1d475a908a877d0a4c124d70792a6ef1
yskim <yesol.kim96@gmail.com>
2023-08-22 17:09:05 +0900

    init source

---
+++ src/main/java/com/example/demo1_jenkins/Demo1JenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo1_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1JenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1JenkinsApplication.class, args);
+    }
+
+}
